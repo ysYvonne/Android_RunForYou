458b839d4f6a4688d62f15a4ec5fbf3ca94d3fa9
IIridescence <592933248@qq.com>
2017-04-24 19:44:56 +0800

    个人信息跳转

---
+++ app/src/main/java/xzh/com/materialdesign/ui/NicknameActivity.java
@@ -0,0 +1,56 @@
+package xzh.com.materialdesign.ui;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.InjectView;
+import xzh.com.materialdesign.R;
+import xzh.com.materialdesign.utils.ActivityHelper;
+
+/**
+ * Created by Flora on 2017/4/24.
+ */
+
+public class NicknameActivity extends AppCompatActivity {
+
+    @InjectView(R.id.nickname_ok_button)
+    Button okButton;
+    @InjectView(R.id.nickname_cancel_button)
+    Button cancelButton;
+
+    private Context mContext;
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nickname_change);
+        mContext = NicknameActivity.this;
+
+        init();
+    }
+
+    private void init(){
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ActivityHelper.startActivity(NicknameActivity.this,PersonalInfoActivity.class);
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                ActivityHelper.startActivity(NicknameActivity.this,PersonalInfoActivity.class);
+            }
+        });
+    }
+}
