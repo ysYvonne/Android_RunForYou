dd103def846c44b703fe15d2ae7c1547aa20974a
Tobit1996 <1070379645@qq.com>
2017-05-18 09:38:23 +0800

    界面美化基本完善（未修改id）

--- app/src/main/java/xzh/com/materialdesign/ui/ModifyActivity.java
+++ app/src/main/java/xzh/com/materialdesign/ui/ModifyActivity.java
@@ -3,6 +3,7 @@ package xzh.com.materialdesign.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 import butterknife.ButterKnife;
@@ -24,7 +25,9 @@ public class ModifyActivity extends BaseActivity {
     Button modify_confirm;
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.modify_custom);
         ButterKnife.inject(this);
         modify_confirm.setOnClickListener(new View.OnClickListener() {
